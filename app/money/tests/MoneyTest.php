b2e74d4e91de2b488c29b45256fa7be359eed690
cureseven <shiningcureseven@gmail.com>
2022-07-31 20:51:58 +0900

    5章

--- app/money/tests/MoneyTest.php
+++ app/money/tests/MoneyTest.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Money\Tests;
 
+use Money\Franc;
 use PHPUnit\Framework\TestCase;
 use Money\Dollar;
 
 class MoneyTest extends TestCase
 {
-    public function testMultiplication()
+    public function testDollarMultiplication()
     {
         $five = new Dollar(5);
         $this->assertEquals(new Dollar(10), $five->times(2));
@@ -21,5 +22,12 @@ class MoneyTest extends TestCase
         $five = new Dollar(5);
         $this->assertTrue($five->equals(new Dollar(5)));
         $this->assertFalse($five->equals(new Dollar(6)));
+    }
+
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+        $this->assertEquals(new Franc(10), $five->times(2));
+        $this->assertEquals(new Franc(15), $five->times(3));
     }
 }
