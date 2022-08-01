7e2899a8d9fc62ffd1a98f44d862c96a35be9bdb
cureseven <shiningcureseven@gmail.com>
2022-08-01 11:33:58 +0900

    11章

--- app/money/tests/MoneyTest.php
+++ app/money/tests/MoneyTest.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Money\Tests;
 
-use Money\Franc;
 use Money\Money;
 use PHPUnit\Framework\TestCase;
-use Money\Dollar;
 
 class MoneyTest extends TestCase
 {
-    public function testDollarMultiplication()
+    public function testMultiplication()
     {
         $five = Money::dollar(5);
         $this->assertEquals(Money::dollar(10), $five->times(2));
@@ -23,26 +21,12 @@ class MoneyTest extends TestCase
         $five = Money::dollar(5);
         $this->assertTrue($five->equals(Money::dollar(5)));
         $this->assertFalse($five->equals(Money::dollar(6)));
-        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
-        $this->assertFalse((Money::franc(5))->equals(Money::franc(6)));
         $this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
-    }
-
-    public function testFrancMultiplication()
-    {
-        $five = Money::franc(5);
-        $this->assertEquals(Money::franc(10), $five->times(2));
-        $this->assertEquals(Money::franc(15), $five->times(3));
     }
 
     public function testCurrency()
     {
         $this->assertEquals("USD", (Money::dollar(1))->currency());
         $this->assertEquals("CHF", (Money::franc(1))->currency());
-    }
-
-    public function testDifferentClassEquality()
-    {
-        $this->assertTrue((new Money(10, "CHF"))->equals(new Franc(10, "CHF")));
     }
 }
