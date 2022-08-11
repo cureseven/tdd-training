c09bfeb34ea0a7e9bfcda658661b772eb26e5edb
cureseven <shiningcureseven@gmail.com>
2022-08-11 11:54:39 +0900

    19章

--- app/xUnit/xunit.py
+++ app/xUnit/xunit.py
@@ -1,31 +1,31 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
     def testMethod(self):
         self.setUp()
         self.wasRun = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        # TestCaseのrunのなかでのself.setUp()はここを呼ぶようになる
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
     def testSetUp(self):
-        # arranage
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
